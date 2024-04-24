4a5555624f11efdbced603137b738fb6ccad0999
Luis Apellaniz <luisape@microsoft.com>
2024-04-23 21:51:20 -0400

    Add unit test for validation

---
+++ function-app/adb-to-purview/tests/unit-tests/Mocks/FakeHttpRequestData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Worker;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace UnitTests.Mocks
+{
+    public class FakeHttpRequestData : HttpRequestData
+    {
+        public FakeHttpRequestData() : this(new FakeFunctionContext(), new Uri("http://fakeurl.test"))
+        {
+
+        }
+
+        public FakeHttpRequestData(FunctionContext functionContext, Uri url, Stream body = null) : base(functionContext)
+        {
+            Url = url;
+            Body = body ?? new MemoryStream();
+        }
+
+        public override Stream Body { get; } = new MemoryStream();
+
+        public override HttpHeadersCollection Headers { get; } = [];
+
+        public override IReadOnlyCollection<IHttpCookie> Cookies { get; }
+
+        public override Uri Url { get; }
+
+        public override IEnumerable<ClaimsIdentity> Identities { get; }
+
+        public override string Method { get; }
+
+        public override HttpResponseData CreateResponse()
+        {
+            return new FakeHttpResponseData(FunctionContext);
+        }
+    }
+
+}
---
+++ function-app/adb-to-purview/tests/unit-tests/Mocks/FakeHttpResponseData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Worker;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Mocks
+{
+    public class FakeHttpResponseData : HttpResponseData
+    {
+        public FakeHttpResponseData(FunctionContext functionContext) : base(functionContext)
+        {
+        }
+
+        public override HttpStatusCode StatusCode { get; set; }
+        public override HttpHeadersCollection Headers { get; set; } = [];
+        public override Stream Body { get; set; } = new MemoryStream();
+        public override HttpCookies Cookies { get; }
+    }
+}
